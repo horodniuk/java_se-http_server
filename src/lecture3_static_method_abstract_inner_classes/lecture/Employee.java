b69e2a88e00b08512e66fc5c5605b7a6167233bf
horodniuk <horodniuk.maksym55@gmail.com>
2022-10-13 19:15:54 +0300

    add template lecture3

---
+++ src/lecture3_static_method_abstract_inner_classes/lecture/Employee.java
@@ -0,0 +1,7 @@
+package lecture3_static_method_abstract_inner_classes.lecture;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("add lection");
+    }
+}
