4b33ce990bac1b43806edf71eb5f02d0712f50b5
horodniuk <horodniuk.maksym55@gmail.com>
2022-10-13 19:22:06 +0300

    delete duplicate class

--- src/lecture3_static_method_abstract_inner_classes/lecture/Employee2.java
+++
@@ -1,7 +0,0 @@
-package lecture3_static_method_abstract_inner_classes.lecture;
-
-public class Employee2 {
-    public static void main(String[] args) {
-        System.out.println("add lection");
-    }
-}
