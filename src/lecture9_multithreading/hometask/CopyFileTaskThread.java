62cc31e92e80a3e3fc9eec670a65565b8ba47420
horodniuk <horodniuk.maksym55@gmail.com>
2022-11-15 23:57:55 +0200

    implement hometask 9 multithreading part1

---
+++ src/lecture9_multithreading/hometask/CopyFileTaskThread.java
@@ -0,0 +1,62 @@
+package lecture9_multithreading.hometask;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CopyFileTaskThread implements Task, Runnable {
+    private Path sourcePath;
+    private Path destinationPath;
+    private long sourseSize;
+    private Thread thread;
+    private volatile long poesentCopy;
+
+    public CopyFileTaskThread(Path sourcePath, Path destinationPath, long sourseSize, Thread thread, long poesentCopy) throws IOException {
+        this.sourcePath = sourcePath;
+        this.destinationPath = destinationPath;
+        this.sourseSize = Files.size(sourcePath);
+        this.thread = new Thread(this, "CopyFileTaskThread");
+        this.poesentCopy = 0;
+    }
+
+    @Override
+    public void start() {
+        if (thread.getState() != Thread.State.NEW){
+            throw new IllegalArgumentException("Copy file already execution");
+        } else {
+            thread.start();
+        }
+    }
+
+    @Override
+    public void interrupt() {
+            this.thread.interrupt();
+    }
+
+    @Override
+    public int getPersentProcesses() {
+        return (int) (this.poesentCopy * 100 / sourseSize);
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[8000];
+        try(InputStream in = new FileInputStream(sourcePath.toFile());
+            OutputStream out = new FileOutputStream(destinationPath.toFile())
+        ){
+            while (!Thread.interrupted()){
+                int read = in.read(buffer);
+                if (read == -1){
+                    break;
+                }
+                out.write(buffer, 0, read);
+                poesentCopy += read;
+            }
+
+        } catch (IOException e) {
+            System.err.println("Copy failed :" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
+
