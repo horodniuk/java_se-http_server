08300e5eebebdc2435d05ceac09d7c64446b08f8
horodniuk <horodniuk.maksym55@gmail.com>
2022-11-27 23:30:10 +0200

    complete task roman number - add method

---
+++ src/main/java/lecture11_unit_test_mockito/hometask/roman_numbner/Main.java
@@ -0,0 +1,10 @@
+package lecture11_unit_test_mockito.hometask.roman_numbner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        final var i = new RomanNumber().romanToInt("V");
+        System.out.println(i);
+    }
+}
